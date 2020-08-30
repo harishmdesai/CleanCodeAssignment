0de39060b2570e1c34299c9ecc2bca62abb27ac6
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-30 17:05:30 +0530

    Update and rename CNN_TemperatureMonitoring.hpp to CNC_TemperatureMonitoring.hpp

---
+++ CNC_TemperatureMonitoring.hpp
@@ -0,0 +1,9 @@
+#ifndef CNC_TEMPERATUREMONITORING_H__
+#define CNC_TEMPERATUREMONITORING_H__
+
+#include "CNC_SupportingFile.hpp"
+#include "CNC_TemperatureRead.hpp"
+
+extern bool Fun_IsTemperatureinRange(void);
+
+#endif
--- CNN_TemperatureMonitoring.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef CNN_TEMPERATUREMONITORING_H__
-#define CNN_TEMPERATUREMONITORING_H__
-
-#include "CNC_SupportingFile.hpp"
-#include "CNN_TemperatureRead.hpp"
-
-extern bool Fun_IsTemperatureinRange(void);
-
-#endif
