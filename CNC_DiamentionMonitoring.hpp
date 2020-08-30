9ec7aaf48eccab78ed916b6668565c250d5e11d0
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-30 17:01:44 +0530

    Update and rename CNN_DiamentionMonitoring.hpp to CNC_DiamentionMonitoring.hpp

---
+++ CNC_DiamentionMonitoring.hpp
@@ -0,0 +1,8 @@
+#ifndef CNC_DIAMENTIONMONITORING_H__
+#define CNC_DIAMENTIONMONITORING_H__
+#include "CNC_SupportingFile.hpp"
+#include "CNC_PartDiamentionVariationRead.hpp"
+
+extern bool Fun_IsDiamentioninRange(void);
+
+#endif
--- CNN_DiamentionMonitoring.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef CNN_DIAMENTIONMONITORING_H__
-#define CNN_DIAMENTIONMONITORING_H__
-#include "CNC_SupportingFile.hpp"
-#include "CNN_PartDiamentionVariationRead.hpp"
-
-extern bool Fun_IsDiamentioninRange(void);
-
-#endif
