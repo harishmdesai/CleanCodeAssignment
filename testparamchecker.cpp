05890acb07353f5319caccab41e4c6f3a3f2aed1
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-30 21:28:44 +0530

    Create testparamchecker.cpp

---
+++ testparamchecker.cpp
@@ -0,0 +1,20 @@
+#include "CNC_DiamentionMonitoring.hpp"
+#include "CNC_DurationMonitoring.hpp"
+#include "CNC_SelfTest.hpp"
+#include "CNC_PartDiamentionVariationRead.hpp"
+#include "CNC_ReadOperationDuration.hpp"
+#include "CNC_TemperatureMonitoring.hpp"
+#include "CNC_TemperatureRead.hpp"
+#include "CNC_SupportingFile.hpp"
+#include "CNC_Main.hpp"
+#include <gtest/gtest.h>
+
+
+TEST(StatusCode, Empty) { 
+    ASSERT_EQ(TRUE, TRUE));
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
