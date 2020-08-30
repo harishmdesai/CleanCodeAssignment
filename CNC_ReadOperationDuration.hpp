433c594da1c1439775e923e566251cb58761521e
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-30 17:04:32 +0530

    Update and rename CNN_ReadOperationDuration.hpp to CNC_ReadOperationDuration.hpp

--- CNC_ReadOperationDuration.hpp
+++ CNC_ReadOperationDuration.hpp
@@ -1,5 +1,5 @@
-#ifndef CNN_READOPERATIONDURATION_H__
-#define CNN_READOPERATIONDURATION_H__
+#ifndef CNC_READOPERATIONDURATION_H__
+#define CNC_READOPERATIONDURATION_H__
 
 extern float CNCDurationOfCNCMachineOperation;
 extern float Fun_ReadCNCMachineDurationinMinutes (void);
