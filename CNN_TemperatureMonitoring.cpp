f207fdc9f67e8586ce5dee7d29485ef13bdad8fa
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-30 14:22:24 +0530

    Create CNN_TemperatureMonitoring.cpp

---
+++ CNN_TemperatureMonitoring.cpp
@@ -0,0 +1,16 @@
+#include "CNN_TemperatureMonitoring.hpp"
+
+/*In Degree centgrade*/
+#define AllowedTemp 35
+
+bool Fun_IsTemperatureinRange(void)
+{
+	bool IsTempInRange = FALSE;
+	float ReadTempvalue;
+	ReadTempvalue = Fun_ReadCNCMachineTemperature();
+	if(WithinALimit(AllowedTemp, ReadTempvalue))
+	{
+		IsTempInRange = TRUE;
+	}
+	return IsTempInRange;
+}
