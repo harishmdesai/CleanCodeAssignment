db1c18e572140fa4b3598340fd7821efcd961c9a
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-30 15:42:01 +0530

    Create CNN_DurationMonitoring.hpp

---
+++ CNN_DurationMonitoring.hpp
@@ -0,0 +1,7 @@
+#ifndef CNN_DURATIONMONITORING_H__
+#define CNN_DURATIONMONITORING_H__
+#include "CNC_SupportingFile.hpp"
+#include "CNN_ReadOperationDuration.hpp"
+extern bool Fun_IsDurationinRange(void);
+
+#endif
