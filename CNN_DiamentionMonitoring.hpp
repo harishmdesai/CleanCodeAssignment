28fb4a2440a188e420d3a78e4a38f44cbba9feae
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-30 15:13:44 +0530

    Update CNN_DiamentionMonitoring.hpp

--- CNN_DiamentionMonitoring.hpp
+++ CNN_DiamentionMonitoring.hpp
@@ -1,6 +1,8 @@
 #ifndef CNN_DIAMENTIONMONITORING_H__
 #define CNN_DIAMENTIONMONITORING_H__
 #include "CNC_SupportingFile.hpp"
+#include "CNN_PartDiamentionVariationRead.hpp"
+
 extern bool Fun_IsDiamentioninRange(void);
 
 #endif
