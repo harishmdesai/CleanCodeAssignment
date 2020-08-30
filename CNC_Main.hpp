7f6f2dd277adc03bf46de9ce8500c183366f2f1f
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-30 18:09:56 +0530

    Update CNC_Main.hpp

--- CNC_Main.hpp
+++ CNC_Main.hpp
@@ -1,6 +1,30 @@
-#ifndef CNC_MAIN_HPP__
-#define CNC_MAIN_HPP__
+#ifndef CNC_MAINHPP__
+#define CNC_MAINHPP__
 
+typedef enum
+{
+	CNCSelfTest,
+	CNCTempMon,
+	CNCDiamMon,
+	CNCDuration,
+	CNCListOfMOn
+}CNCListOfMOn;
+
+typedef enum
+{
+	CNC_AllOk,
+	CNC_MachineAlert,
+	CNC_Env_Alert,
+	CNC_MachineandEnvAlert
+}CNC_DiagAlert;
+
+typedef struct
+{
+	CNCSelfTestStatus SelfTest;
+  float temp;
+	float Diam;
+  float Duraion;
+}CNCParameter;
 
 
 #endif
