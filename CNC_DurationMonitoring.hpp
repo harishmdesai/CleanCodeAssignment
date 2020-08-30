86711314c0579f2392b35b3285f6075cbb95059b
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-30 17:02:44 +0530

    Update and rename CNN_DurationMonitoring.hpp to CNC_DurationMonitoring.hpp

---
+++ CNC_DurationMonitoring.hpp
@@ -0,0 +1,7 @@
+#ifndef CNC_DURATIONMONITORING_H__
+#define CNC_DURATIONMONITORING_H__
+#include "CNC_SupportingFile.hpp"
+#include "CNC_ReadOperationDuration.hpp"
+extern bool Fun_IsDurationinRange(void);
+
+#endif
--- CNN_DurationMonitoring.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef CNN_DURATIONMONITORING_H__
-#define CNN_DURATIONMONITORING_H__
-#include "CNC_SupportingFile.hpp"
-#include "CNN_ReadOperationDuration.hpp"
-extern bool Fun_IsDurationinRange(void);
-
-#endif
