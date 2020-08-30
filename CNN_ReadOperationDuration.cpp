1cde835e70adae968d279464c73a4279debb410d
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-30 15:39:01 +0530

    Create CNN_ReadOperationDuration.cpp

---
+++ CNN_ReadOperationDuration.cpp
@@ -0,0 +1,16 @@
+#include "CNN_ReadOperationDuration.hpp"
+
+float CNCDurationOfCNCMachineOperation;
+
+/*Resolution can be changed based on teh requirment*/
+static float FUN_GetCNCMachineOperatingDuration(void)
+{ 
+   return CNCDurationOfCNCMachineOperation;
+}
+
+float Fun_ReadCNCMachineDurationinMinutes(void)
+{
+	float temp;
+	temp = FUN_GetCNCMachineOperatingDuration();
+	return temp;
+}
