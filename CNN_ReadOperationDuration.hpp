4b87922534b4f194c87af8fd86944c1387f86d16
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-30 15:40:23 +0530

    Create CNN_ReadOperationDuration.hpp

---
+++ CNN_ReadOperationDuration.hpp
@@ -0,0 +1,7 @@
+#ifndef CNN_READOPERATIONDURATION_H__
+#define CNN_READOPERATIONDURATION_H__
+
+extern float CNCDurationOfCNCMachineOperation;
+extern float Fun_ReadCNCMachineDurationinMinutes (void);
+
+#endif
