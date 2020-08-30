b1822995cc19939f8d3f6cde2f86b2aedb988ac4
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-30 17:06:25 +0530

    Update CNN_TemperatureRead.hpp

--- CNN_TemperatureRead.hpp
+++ CNN_TemperatureRead.hpp
@@ -1,5 +1,5 @@
-#ifndef CNN_TEMPERATUREREAD_HPP__
-#define CNN_TEMPERATUREREAD_HPP__
+#ifndef CNC_TEMPERATUREREAD_HPP__
+#define CNC_TEMPERATUREREAD_HPP__
 
 extern float CNCMachineTemperatre;
 extern float Fun_ReadCNCMachineTemperature (void);
