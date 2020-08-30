62e1f6460bfc269cc91bc3a869b4ec3efa2ca690
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-30 15:09:02 +0530

    Create CNN_DiamentionMonitoring.cpp

---
+++ CNN_DiamentionMonitoring.cpp
@@ -0,0 +1,15 @@
+#include "CNN_DiamentionMonitoring.hpp"
+
+#define AllowedDiam 0.05 //in mm
+
+bool Fun_IsDiamentioninRange(void)
+{
+	bool IsDiamentionInRange = FALSE;
+	float ReadDiamentionvalue;
+	ReadDiamentionvalue = Fun_ReadCNCMachineDiamention();
+	if(WithinALimit(AllowedDiam, ReadDiamentionvalue))
+	{
+		IsDiamentionInRange = TRUE;
+	}
+	return IsDiamentionInRange;
+}
