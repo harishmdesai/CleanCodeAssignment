5dabe13fde2d71d283b6446679fdb6bbc88e6fef
harishmdesai <68503258+harishmdesai@users.noreply.github.com>
2020-08-31 14:27:58 +0530

    Update CNC_PartDiamentionVariationRead.hpp

--- CNC_PartDiamentionVariationRead.hpp
+++ CNC_PartDiamentionVariationRead.hpp
@@ -2,6 +2,7 @@
 #define CNC_PARTDIAMENTIONVARIATIONREAD_H__
 
 extern float CNCPartDiamentionVariationValue;
+extern float FUN_ADCRead_DiamentionVariation (float Value);
 extern float Fun_ReadCNCMachineDiamention (void);
 
 #endif
